8062e84efe5eb465916e37d9997e44023f41e841
Michael Hoppes <mhoppes@i3logix.com>
2019-05-20 13:04:15 -0600

    wordy

--- wordy/wordy.php
+++ wordy/wordy.php
@@ -3,5 +3,41 @@ declare(strict_types=1);
 
 function calculate(string $input): int
 {
+    // Strip "What is " and "?"
+    $process = str_replace(['What is ', '?'], '', $input);
 
+    $array = explode(' ', $process);
+    // Get value
+    $first = (int) array_shift($array);
+
+    while (count($array) > 0) {
+        // Get operator
+        $op = array_shift($array);
+        // If Multiply or Divide, extra pop the by
+        if ($op === 'multiplied' || $op === 'divided') {
+            array_shift($array);
+        }
+
+        // Get value
+        $second = (int) array_shift($array);
+
+        switch ($op) {
+            case 'plus':
+                $first += $second;
+                break;
+            case 'minus':
+                $first -= $second;
+                break;
+            case 'multiplied':
+                $first *= $second;
+                break;
+            case 'divided':
+                $first /= $second;
+                break;
+            default:
+                throw new InvalidArgumentException;
+        }
+    }
+
+    return $first;
 }
