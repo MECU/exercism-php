d75ce4aab69f36b5631db63ab756b09c559359a1
Michael Hoppes <mhoppes@i3logix.com>
2019-05-20 11:51:34 -0600

    refactor

--- luhn/luhn.php
+++ luhn/luhn.php
@@ -12,23 +12,21 @@ function isValid(string $rawInput): bool
     }
 
     // strings of length 0 or 1 are invalid
-    if (strlen($input) < 2) {
+    $length = strlen($input);
+    if ($length < 2) {
         return false;
     }
 
-    // Cycle through the string from the right
-    $array = array_reverse(str_split($input));
-    foreach ($array as $key => &$value) {
-        // Double even values, subtract 9 if doubled value > 9
-        if ($key % 2 !== 0) {
-            $value *= 2;
-            if ($value > 9) {
-                $value -= 9;
-            }
+    // Every 2nd char through the string from the right
+    for ($i = $length - 2; $i > -1; $i -= 2) {
+        // Double values, subtract 9 if doubled value > 9
+        $value = $input[$i] * 2;
+        if ($value > 9) {
+            $value -= 9;
         }
+        $input[$i] = $value;
     }
 
-    // Sum all the values
     // If the sum of all values is evenly divisible by 10: valid
-    return array_sum($array) % 10 === 0;
+    return array_sum(str_split($input)) % 10 === 0;
 }
