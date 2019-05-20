c7b09cb76c727070362cc6360f558f50a3cc60c0
Michael Hoppes <mhoppes@i3logix.com>
2019-05-20 13:35:24 -0600

    accumulate

---
+++ accumulate/accumulate.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+function accumulate(array $input, callable $accumulator): array
+{
+    foreach ($input as &$value) {
+        $value = $accumulator($value);
+    }
+
+    return $input;
+}
